e92acec9a36ad97b18b68fd3aa83c014be894d78
Sam Lee <saml1087@hstat.org>
2021-03-08 15:44:51 +0000

    update repo

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Set-up
+var userBorough = prompt("Which borough population do you want to learn about?")
+
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/xywu-7bv9.json',
     dataType: 'json',
     success: function(data) {
-        console.log(data);
+        // console.log(data);
+        console.log("test")
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].borough == ("   " + userBorough)){
+                var population = (data[i]["_2020"])
+                var percentage = (data[i]["_2020_boro_share_of_nyc_total"])
+                document.write("There are " + population + " people in " + userBorough + "<br>")
+                document.write("This is the percentage of the NYC Total Population: " + percentage + "% <br>")
+            }
+        }
     }
 });
