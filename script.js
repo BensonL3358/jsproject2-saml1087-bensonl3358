1e24b4c2ffd00644be79952d6b0c35f53f4680da
Sam Lee <saml1087@hstat.org>
2021-03-10 15:34:45 +0000

    update repo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set-up
 var userBorough = prompt("Which borough population do you want to learn about?")
-
+var userYear = prompt("What year of the population do you want to see (1950 - 2040) (Enter by 10s)")
 $.ajax({
     url: 'https://data.cityofnewyork.us/resource/xywu-7bv9.json',
     dataType: 'json',
@@ -9,9 +9,9 @@ $.ajax({
         console.log("test")
         for (var i = 0; i < data.length; i++) {
             if (data[i].borough == ("   " + userBorough)){
-                var population = (data[i]["_2020"])
-                var percentage = (data[i]["_2020_boro_share_of_nyc_total"])
-                document.write("There are " + population + " people in " + userBorough + "<br>")
+                var population = (data[i]["_" + userYear])
+                var percentage = (data[i]["_" + userYear + "_boro_share_of_nyc_total"])
+                document.write("There are " + population + " people in " + userBorough + " in " + userYear + "<br>")
                 document.write("This is the percentage of the NYC Total Population: " + percentage + "% <br>")
             }
         }
