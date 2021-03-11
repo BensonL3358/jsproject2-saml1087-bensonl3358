89feafa5e07287828dfdab463a7888953f9c1f8e
Benson Liao <BensonL3358@hstat.org>
2021-03-11 02:35:54 +0000

    update repo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set-up (Storing User Input)
-var userBorough = prompt("Which borough population do you want to learn about?")
-var userYear = prompt("What year of the population do you want to see (1950 - 2040) (Enter by 10s)")
+var userBorough = prompt("Which borough population do you want to learn about?");
+var userYear = prompt("What year of the population do you want to see (1950 - 2040) (Enter by 10s)");
 
 //API Grabber
 $.ajax({
@@ -8,14 +8,16 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         // console.log(data);
-        console.log("test")
+        console.log("test");
         for (var i = 0; i < data.length; i++) { // Loops through data array
             if (data[i].borough == ("   " + userBorough)){ // Checks if borough is equal to userBorough
-                var population = (data[i]["_" + userYear]) // Grabs Property called _(userYear)
-                var percentage = (data[i]["_" + userYear + "_boro_share_of_nyc_total"]) //Grabs property called _(userYear)_boro_share_of_nyc_total
+                var population = (data[i]["_" + userYear]); // Grabs Property called _(userYear)
+                var percentage = (data[i]["_" + userYear + "_boro_share_of_nyc_total"]); //Grabs property called _(userYear)_boro_share_of_nyc_total
 
-                document.write("There are " + population + " people in " + userBorough + " in " + userYear + ".<br>")
-                document.write("This is " + percentage + "% of the total population in NYC. <br>")
+                document.write('<center>'); // Centers text after user inputs their answers
+                document.write("There are " + population + " people in " + userBorough + " in " + userYear + ".<br>");
+                document.write("This is " + percentage + "% of the total population in NYC. <br>");
+                document.body.style.backgroundColor = "#add8e6"; // Changes background color after user inputs answers
             }
         }
     }
